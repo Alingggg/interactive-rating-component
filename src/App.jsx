32cf30635486bf50a837abdb1ca8b579d77059d3
Alingggg <lapitangeof2@gmail.com>
2024-11-17 21:20:23 +0800

    finish app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import Rating from "./components/Rating";
+import ThankYou from "./components/ThankYou";
+
 function App() {
+  const [ratingValue, setRatingValue] = useState(null);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Rating
+        ratingValue={ratingValue}
+        setRatingValue={setRatingValue}
+        isSubmitted={isSubmitted}
+        setIsSubmitted={setIsSubmitted}
+      ></Rating>
+      <ThankYou ratingValue={ratingValue} isSubmitted={isSubmitted}></ThankYou>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        lightGray: "hsl(217, 12%, 63%)",
+        darkBlue: "hsla(213, 19%, 18%, 0.7)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+        lowOpacityGray: "hsla(217, 12%, 63%, 0.1)",
+        lowOpacityOrange: "hsla(25, 97%, 53%, 0.8)",
+      },
+      fontFamily: {
+        overpass: "overpass",
+      },
+    },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
-
