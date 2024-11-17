5c4e485c7c3e47a740865c5dd3017e78e7d21e67
Alingggg <lapitangeof2@gmail.com>
2024-11-17 21:37:31 +0800

    fix github deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Alingggg/interactive-rating-component",
+  base: "/interactive-rating-component",
   plugins: [react()],
 });
